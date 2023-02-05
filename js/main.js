6e1fed8b8caeced5afbc12653899ad6e73c515cb
Simone Marino <simo.marino@outlook.it>
2023-02-05 14:07:16 +0100

    Counter Time's up fixed

--- js/main.js
+++ js/main.js
@@ -58,5 +58,9 @@ function myCountDown() {
   if (timeLeft < 0) {
     clearInterval(count);
     document.getElementById("title").innerHTML = "TIME'S UP!!!";
+    document.getElementById("seconds").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("days").innerHTML = "00";
   }
 }
